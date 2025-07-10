2c4e0a2ccecd5aec74ba3a73147eb6059761a059
adrshkmr <education@erased.aleeas.com>
2025-07-10 18:39:38 +0530

    Added: browser-configs, wofi

--- waybar/.config/waybar/scripts/network_speed.py
+++ waybar/.config/waybar/scripts/network_speed.py
@@ -11,7 +11,7 @@ up = now.bytes_sent - prev.bytes_sent
 def format_speed(bytes_per_sec):
     kb = bytes_per_sec / 1024
     mb = kb / 1024
-    return f"{mb:.1f} MB/s" if mb >= 1 else f"{kb:.0f} KB/s"
+    return f"{mb:.1f}M" if mb >= 1 else f"{kb:.0f}K "
 
 # FontAwesome: Upload = \uf062, Download = \uf063
 print(f"\uf063 {format_speed(down)}  \uf062 {format_speed(up)}")
