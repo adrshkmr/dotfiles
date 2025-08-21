812b5bfd1be07c07ce32729bf638beda63f60ef7
adrshkmr <education@erased.aleeas.com>
2025-08-21 23:04:03 +0530

    small changes in plugins

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -12,15 +12,33 @@ vim.keymap.set("n", "<C-j>", "<Cmd>wincmd j<CR>", {})
 vim.keymap.set("n", "<C-k>", "<Cmd>wincmd k<CR>", {})
 vim.keymap.set("n", "<C-l>", "<Cmd>wincmd l<CR>", {})
 
--- Navigate better in Terminal mode
+-- -- Navigate better in Terminal mode
+-- function _G.set_terminal_keymaps()
+-- 	local opts = { noremap = true }
+-- 	vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+-- 	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+-- 	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+-- 	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+-- 	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+-- 	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+-- end
+--
+-- vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
 function _G.set_terminal_keymaps()
 	local opts = { noremap = true }
-	vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+	local map = vim.api.nvim_buf_set_keymap
+	map(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+	map(0, "t", "jk", [[<C-\><C-n>]], opts)
+	map(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+	map(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+	map(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+	map(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
-vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "term://*",
+	callback = function()
+		pcall(set_terminal_keymaps) -- no crash even if buffer invalid
+	end,
+})
